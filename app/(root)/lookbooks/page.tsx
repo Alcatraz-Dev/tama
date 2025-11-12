5f81925cd8a2b694f7e6c1173f31de192adeaae8
Haythem Dhahri <alcatraz3019@gmail.com>
2025-11-12 20:41:14 +0100

    update the theme and fix the bugs

--- app/(root)/lookbooks/page.tsx
+++ app/(root)/lookbooks/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { getLookbook } from "@/lib/useQuery";
-
+import { motion } from "framer-motion";
 
 export default async function LookbooksPage() {
   const lookbooks = await getLookbook();
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,8 +7,6 @@ import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Button } from "./ui/button";
 import { CiMenuFries } from "react-icons/ci";
-import Image from "next/image";
-import Logo from "../public/tama.svg"
 const links = [
   { href: "/products", label: "Products" },
   { href: "/about", label: "About" },
@@ -30,13 +28,14 @@ export default function Navbar() {
       initial={{ y: -50, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.5 }}
-      className={`sticky top-0 z-50 backdrop-blur-sm transition-all duration-300 ${scrolled ? "shadow-md bg-white/70" : "bg-transparent"
-        }`}
+      className={`sticky top-0 z-50 backdrop-blur-sm transition-all duration-300 ${
+        scrolled ? "shadow-md bg-white/70" : "bg-transparent"
+      }`}
     >
       <div className="container mx-auto flex items-center justify-between py-4 px-6 relative">
         {/* Hamburger menu */}
         <Button className="md:hidden hover:cursor-pointer w-8 h-8 ">
-          <CiMenuFries className="w-3 h-3  hover:text-black" />
+         <CiMenuFries className="w-3 h-3  hover:text-black" />
         </Button>
 
         {/* Center Logo */}
@@ -44,14 +43,8 @@ export default function Navbar() {
           href="/"
           className="absolute left-1/2 transform -translate-x-1/2 text-4xl tracking-wide font-indie-flower uppercase font-extrabold"
         >
-          <Image
-            src={Logo}
-            alt="logo"
-            className="md:w-[150px] md:h-[150px]  w-[100px] h-[100px]"
-
-          />
+          Tama
         </Link>
-
 
         {/* Right side nav */}
         <div className="flex items-center gap-6 ml-auto">
@@ -73,8 +66,8 @@ export default function Navbar() {
           </nav>
 
           {/* Cart */}
-          <Link href="/cart" className={`relative  p-2 rounded-lg ${scrolled ? "bg-black" : " bg-white"}`}>
-            <ShoppingBasket className={`w-5 h-5   ${scrolled ? " text-white hover:text-gray-700" : "text-gray-700 hover:text-black"}`} />
+          <Link href="/cart" className="relative bg-white p-2 rounded-lg">
+            <ShoppingBasket className="w-5 h-5 text-gray-700 hover:text-black" />
             <AnimatePresence>
               {cartItems.length > 0 && (
                 <motion.span
@@ -82,7 +75,7 @@ export default function Navbar() {
                   initial={{ scale: 0 }}
                   animate={{ scale: 1 }}
                   exit={{ scale: 0 }}
-                  className={`absolute -top-2 -right-2  text-xs w-5 h-5 rounded-full flex items-center justify-center ${scrolled ? "bg-white text-black drop-shadow-2xl" : " bg-black text-white drop-shadow-2xl"}`}
+                  className="absolute -top-2 -right-2 bg-black text-white text-xs w-5 h-5 rounded-full flex items-center justify-center"
                 >
                   {cartItems.length}
                 </motion.span>
