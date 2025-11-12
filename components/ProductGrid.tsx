87c07b2e5d4e06900cc726306aad4e58740c98bd
Haythem Dhahri <alcatraz3019@gmail.com>
2025-11-12 19:05:34 +0100

    fix import componets

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -1,6 +1,7 @@
 import { getProducts } from "@/lib/useQuery";
 import ClientProductGrid from "./ClientProductGrid";
 
+
 export default async function ProductGrid() {
   const products = await getProducts();
 
